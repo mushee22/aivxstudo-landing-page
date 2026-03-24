c655fc924d94cbdc5ba9f2a6fc22a0bb47d972c9
mushee22 <mmtmusheer@gmail.com>
2026-03-24 11:14:12 +0530

    feat: add Microsoft Clarity tracking script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,21 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=G-6XT7PG56LG"
           strategy="afterInteractive"
         />
+
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w07rq3eydi");
+            `
+          }}
+        />
+
         <Script
           id="gtm-script"
           strategy="afterInteractive"
